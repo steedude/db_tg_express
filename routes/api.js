ada1d2aa73481b21bea5961877045c1f83524caa
jason <jason.wu@skylinetw.com>
2023-08-01 16:35:02 +0800

    log fetch

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ router.get("/", async (req, res, next) => {
   setTimeout((() => {
     fetch("https://api.telegram.org/bot6359990785:AAGNEIUUwgdSK2BtF2PWgqj9i--v_XzDZXQ/sendMessage?chat_id=635999134&text=Hello")
   }), 3000)
+  console.log(fetch);
   return res.status(200).json({
     title: "Express Testing......",
     message: "The app is working properly!",
