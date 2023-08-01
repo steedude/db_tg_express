41e1c6d6a14302760046e565e2de9c40f6cfb156
jason <jason.wu@skylinetw.com>
2023-08-01 17:11:07 +0800

    10分鐘

--- routes/api.js
+++ routes/api.js
@@ -8,12 +8,11 @@ router.get("/", async (req, res, next) => {
       setTimeout((() => {
         fetch("https://api.telegram.org/bot6359990785:AAGNEIUUwgdSK2BtF2PWgqj9i--v_XzDZXQ/sendMessage?chat_id=635999134&text=QQQQQQ")
           .then(resolve)
-      }), 3000)
+      }), 10*60*1000)
     })  
   }
 
   await telebotPromise();
-  console.log(fetch);
   return res.status(200).json({
     title: "Express Testing......",
     message: "The app is working properly!",
