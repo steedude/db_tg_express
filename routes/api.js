b36cbd78e869441f77e02a49c4982abcaab2931b
jason <jason.wu@skylinetw.com>
2023-08-01 17:00:36 +0800

    fetch

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const axios = require('axios');
+// const axios = require('axios');
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   setTimeout((() => {
-    axios("https://api.telegram.org/bot6359990785:AAGNEIUUwgdSK2BtF2PWgqj9i--v_XzDZXQ/sendMessage?chat_id=635999134&text=Hello")
+    fetch("https://api.telegram.org/bot6359990785:AAGNEIUUwgdSK2BtF2PWgqj9i--v_XzDZXQ/sendMessage?chat_id=635999134&text=QQQQQQ")
   }), 3000)
   console.log(fetch);
   return res.status(200).json({
