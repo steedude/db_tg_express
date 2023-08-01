b2ee813f5cc9b915f558450b469f527fc56d6342
jason <jason.wu@skylinetw.com>
2023-08-01 17:08:46 +0800

    promise

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  setTimeout((() => {
-    fetch("https://api.telegram.org/bot6359990785:AAGNEIUUwgdSK2BtF2PWgqj9i--v_XzDZXQ/sendMessage?chat_id=635999134&text=QQQQQQ")
-  }), 3000)
+  function telebotPromise() {
+    return new Promise((resolve) => {
+      setTimeout((() => {
+        fetch("https://api.telegram.org/bot6359990785:AAGNEIUUwgdSK2BtF2PWgqj9i--v_XzDZXQ/sendMessage?chat_id=635999134&text=QQQQQQ")
+          .then(resolve)
+      }), 3000)
+    })  
+  }
+
+  await telebotPromise();
   console.log(fetch);
   return res.status(200).json({
     title: "Express Testing......",
