36ff805bf7d18839d66627ca70ba87b6ed7e3908
jason <jason.wu@skylinetw.com>
2023-08-01 16:19:29 +0800

    改成我的機器人

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   setTimeout((() => {
-    fetch("https://api.telegram.org/bot6345491638:AAGZxW09Z5PKQ-Zwvptwnpfv7FoGfNPa7q4/sendMessage?chat_id=263995857&text=Hello")
-  }), 1000*60)
+    fetch("https://api.telegram.org/bot6359990785:AAGNEIUUwgdSK2BtF2PWgqj9i--v_XzDZXQ/sendMessage?chat_id=635999134&text=Hello")
+  }), 3000)
   return res.status(200).json({
-    title: "Express Testing",
+    title: "Express Testing......",
     message: "The app is working properly!",
   });
 });
