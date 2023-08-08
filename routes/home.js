6649b9890719c8177ac335871ad4ee814bffdb08
jason <jason.wu@skylinetw.com>
2023-08-08 16:44:07 +0800

    修正按鈕顯示問題

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,7 @@ const moment = require('moment');
 const { bot } = require("../utils/bot.js");
 const { client } = require("../utils/db.js");
 const schedule = require('node-schedule');
-const { Markup } = require('telegraf')
-const { session, Scenes } = require('telegraf');
+const { session, Scenes, Markup } = require('telegraf');
 const Stage = Scenes.Stage
 const WizardScene = Scenes.WizardScene
 
@@ -24,6 +23,10 @@ bot.hears('上班', ctx => {
   const job = schedule.scheduleJob(newTime, function(){
     bot.telegram.sendMessage(chatId, '辛苦了，下班囉');
   });
+})
+bot.hears('dd', ctx => {
+  const date = new Date(2012, 11, 21, 5, 30, 0);
+  console.log(date)
 })
 
 
@@ -97,9 +100,12 @@ const reserveWizard = new WizardScene(
     ctx.wizard.state.data.month = ctx.message.text;
     ctx.reply('請輸入日期', Markup
     .keyboard([
-      ['01號', '02號', '03號', '04號','05號', '06號', '07號', '08號','09號', '10號'],
-      ['11號', '12號', '13號', '14號','15號', '16號', '17號', '18號','19號', '20號'],
-      ['21號', '22號', '23號', '24號','25號', '26號', '27號', '28號','29號', '30號','31號'],
+      ['01號', '02號', '03號', '04號', '05號'],
+      ['06號', '07號', '08號', '09號', '10號'],
+      ['11號', '12號', '13號', '14號', '15號'],
+      ['16號', '17號', '18號', '19號', '20號'],
+      ['21號', '22號', '23號', '24號', '25號'],
+      ['26號', '27號', '28號', '29號', '30號', '31號']
     ])
     .oneTime()
     .resize()
@@ -110,8 +116,9 @@ const reserveWizard = new WizardScene(
     ctx.wizard.state.data.date = ctx.message.text;
     ctx.reply('請輸入小時', Markup
     .keyboard([
-      ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11','12'],
-      ['13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24']
+      ['00', '01', '02', '03', '04', '05', '06', '07'],
+      ['08', '09', '10', '11', '12', '13', '14', '15'],
+      ['16', '17', '18', '19', '20', '21', '22', '23']
     ])
     .oneTime()
     .resize()
@@ -120,16 +127,35 @@ const reserveWizard = new WizardScene(
   },
   ctx => {
     ctx.wizard.state.data.hour = ctx.message.text;
-    ctx.reply('請輸入分鐘')
+    ctx.reply('請輸入分鐘', Markup
+    .keyboard([
+      ['00', '01', '02', '03', '04', '05', '06', '07'],
+      ['08', '09', '10', '11', '12', '13', '14', '15'],
+      ['16', '17', '18', '19', '20', '21', '22', '23'],
+      ['24', '25', '26', '27', '28', '29', '30', '31'],
+      ['32', '33', '34', '35', '36', '37', '38', '39'],
+      ['40', '41', '42', '43', '44', '45', '46', '47'],
+      ['48', '49', '50', '51', '52', '53', '54', '55'],
+      ['56', '57', '58', '59']
+    ])
+    .oneTime()
+    .resize())
+    return ctx.wizard.next();
+  },
+  ctx => {
+    ctx.wizard.state.data.mins = ctx.message.text;
+    ctx.reply('請輸入提醒內容')
     return ctx.wizard.next();
   },
   async ctx =>  {
-    if (ctx.message.text == 'cancel') {
-      return ctx.scene.leave();
-    }
-    ctx.wizard.state.data.mins = ctx.message.text;
+    ctx.wizard.state.data.msg = ctx.message.text;
     let datas = ctx.wizard.state.data
-    ctx.reply(`${datas.month.slice(0,2)}/${datas.date.slice(0,2)} ${datas.hour}:${datas.mins}`)
+    const date = moment(`2023-${datas.month.slice(0, 2)}-${datas.date.slice(0, 2)}, ${datas.hour}:${datas.mins}`, "YYYY-MM-DD, hh:mm").format('YYYY-MM-DD, hh:mm');
+    const job = schedule.scheduleJob(date, function(){
+      bot.telegram.sendMessage(ctx.chat.id, datas.msg);
+    })
+    ctx.reply(`將於${date}提醒${datas.msg}`)
+    return ctx.scene.leave();
   }
 );
 const stage = new Stage([registerWizard,reserveWizard]);
