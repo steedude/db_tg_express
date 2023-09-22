a8eb0431aed447f52d490466297dd484243dc4bd
steedude <85280987+steedude@users.noreply.github.com>
2023-09-22 14:52:12 +0800

    只回resultCode

--- routes/api.js
+++ routes/api.js
@@ -12,51 +12,33 @@ router.post('/register', async (req, res) => {
   const { username, password, confirmPassword } = req.body
   //防呆
   if (!username || !password || !confirmPassword) {
-    return res
-      .status(400)
-      .json({ title: 'error', message: 'missing information' })
+    return res.status(400).json({ resultCode: 401 })
   }
   if (password !== confirmPassword) {
-    return res.status(400).json({
-      title: 'error',
-      message: 'password and confirmPassword are not the same',
-    })
+    return res.status(400).json({ resultCode: 402 })
   }
-  //確認有無重複帳號
   try {
+    //確認有無重複帳號
     const searchResult = await User.findOne({ username: username })
     if (searchResult != null) {
-      return res.status(400).json({
-        title: 'error',
-        message: 'Duplicate username',
-      })
+      return res.status(400).json({ resultCode: 403 })
     }
-  } catch (e) {
-    return res.status(400).json({
-      title: 'error',
-      message: e,
-    })
-  }
-  //新增帳號
-  const doc = {
-    username: username,
-    password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
-  }
-  try {
+    //新增帳號
+    const doc = {
+      username: username,
+      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
+    }
     const insertResult = await User.create(doc)
     return res.status(200).json({
-      title: 'success',
-      message: 'A document was inserted',
+      resultCode: 200,
       resultMap: {
         token: jwt.sign({ id: insertResult._id }, process.env.JWT_SECRET),
       },
     })
-  } catch (e) {
-    console.log(e)
-    return res.status(400).json({
-      title: 'error',
-      message: e,
-    })
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ resultCode: 400, resultMap: { message: err } })
   }
 })
 
@@ -65,30 +47,28 @@ router.post('/login', async (req, res) => {
   const { username, password } = req.body
   //防呆
   if (!username || !password) {
-    return res
-      .status(400)
-      .json({ title: 'error', message: 'missing information' })
+    return res.status(400).json({ resultCode: 401 })
   }
   try {
     const searchResult = await User.findOne({ username: username })
+    //帳號或密碼錯誤
     if (
       searchResult == null ||
       !bcrypt.compareSync(password, searchResult.password)
     ) {
-      return res
-        .status(400)
-        .json({ title: 'error', message: 'Incorrect username or password' })
+      return res.status(400).json({ resultCode: 404 })
     } else {
       return res.json({
-        title: 'success',
-        message: 'login success',
+        resultCode: 200,
         resultMap: {
           token: jwt.sign({ id: searchResult._id }, process.env.JWT_SECRET),
         },
       })
     }
   } catch (err) {
-    return res.status(400).json({ title: 'error', message: err })
+    return res
+      .status(400)
+      .json({ resultCode: 400, resultMap: { message: err } })
   }
 })
 
@@ -101,8 +81,7 @@ router.get(
   async (req, res) => {
     console.log(req.user.username + ' get data')
     return res.status(200).json({
-      title: 'success',
-      message: 'get data success',
+      resultCode: 200,
       resultMap: {
         data: [1, 2, 3, 4],
       },
